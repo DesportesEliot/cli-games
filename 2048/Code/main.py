c57fa716ad6045cc1f042f8e6eff8019b800f36b
myriam.petit <myriam.petit@etu.univ-tlse2.fr>
2026-06-09 15:01:55 +0200

    feat: add main game loop

--- 2048/Code/main.py
+++ 2048/Code/main.py
@@ -2,13 +2,17 @@ from game import Game2048
 
 def main():
     print("=== Bienvenue dans 2048 ===")
-    
+
     jeu = Game2048()
+
     jeu.add_random_tile()
     jeu.add_random_tile()
 
-    for ligne in jeu.get_grid():
-        print(ligne)
+    running = True
+
+    while running:
+        jeu.print_board()
+        running = False
 
 if __name__ == "__main__":
     main()
